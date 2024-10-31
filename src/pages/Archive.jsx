447a5389d26be5ed1e80a17c8bfff7e337c37f9a
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-10-31 12:49:40 +0600

    if no task in archive shoing a text

--- src/pages/Archive.jsx
+++ src/pages/Archive.jsx
@@ -3,19 +3,26 @@ import TaskCard from '../components/tasks/TaskCard';
 
 const Archive = () => {
   const { tasks } = useSelector((state) => state.tasksSlice);
-
-  const archiveTasks = tasks.filter((item) => item.status == 'archive');
+  
+  // Filter tasks to get archived ones
+  const archiveTasks = tasks.filter((item) => item.status === 'archive');
 
   return (
     <div className="p-10">
       <div>
-        <h1 className="text-xl font-semibold mb-10">Archive board</h1>
+        <h1 className="text-xl font-semibold mb-10">Archive Board</h1>
       </div>
-      <div className="grid grid-cols-4 gap-5">
-        {archiveTasks?.map((item) => (
-          <TaskCard key={item.id} task={item} />
-        ))}
-      </div>
+      {archiveTasks.length === 0 ? (
+        <div className="text-center text-gray-600 mt-10">
+          <p>No archived tasks found.</p>
+        </div>
+      ) : (
+        <div className="grid grid-cols-4 gap-5">
+          {archiveTasks.map((item) => (
+            <TaskCard key={item.id} task={item} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
