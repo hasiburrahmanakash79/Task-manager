e115318a42e00d548b14f19dfae2cf3eda5b3fed
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-11-06 17:35:55 +0600

    make another user profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -87,27 +87,53 @@ const Profile = () => {
               Web Developer
             </h1>
             <div className="flex gap-3 items-center">
-              <img
-                className="h-4"
-                src="https://www.pngkey.com/png/full/931-9311379_email-icons-white-color-white-email-icon-png.png"
-                alt=""
-              />
-              <img
-                className="h-5"
-                src="https://www.freeiconspng.com/thumbs/linkedin-logo-png/displaying-19-gallery-images-for-linkedin-logo-png-25.png"
-                alt=""
-              />
-              <img
-                className="h-4"
-                src="https://www.edigitalagency.com.au/wp-content/uploads/Facebook-icon-white-PNG-large-size.png"
-                alt=""
-              />
+              <a
+                href="mailto:hasiburrahmanakash79@gmail.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  className="h-4"
+                  src="https://cdn.icon-icons.com/icons2/2642/PNG/512/google_mail_gmail_logo_icon_159346.png"
+                  alt=""
+                />
+              </a>
+              <a
+                href="fb"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  className="h-5"
+                  src="https://www.freeiconspng.com/thumbs/linkedin-logo-png/displaying-19-gallery-images-for-linkedin-logo-png-25.png"
+                  alt=""
+                />
+              </a>
+              <a
+                href="li"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  className="h-4"
+                  src="https://www.edigitalagency.com.au/wp-content/uploads/Facebook-icon-white-PNG-large-size.png"
+                  alt=""
+                />
+              </a>
               <p>/ hasiburRahmanAkash</p>
             </div>
           </div>
           <div className="flex items-center gap-4">
-            <img className="h-6" src="https://iconape.com/wp-content/png_logo_vector/tailwind-css-logo.png" alt="" />
-            <img className="h-6" src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png" alt="" />
+            <img
+              className="h-6"
+              src="https://iconape.com/wp-content/png_logo_vector/tailwind-css-logo.png"
+              alt=""
+            />
+            <img
+              className="h-6"
+              src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"
+              alt=""
+            />
           </div>
         </div>
       </div>
